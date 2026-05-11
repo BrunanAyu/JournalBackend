48accd5756f5c6efc5f87a357fd04ad369b6f3d8
Bre Ayu <brhanuayalew7@gmail.com>
2026-05-11 00:59:00 -0700

    fix cors error

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://journal-frontend-livid.vercel.app", credentials: true }));
 app.use(express.json());
 
 // Routes
